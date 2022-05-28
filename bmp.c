8c036a08a56e11d4014476627f53c8d46e0d93fd
Nijat Hamidov <hamidov9jat@gmail.com>
2022-05-28 10:52:02 +0400

    added descriptive comments and dib header

--- bmp.c
+++ bmp.c
@@ -9,7 +9,7 @@ void open_bmp_file(const char filename[]) {
 
     printf("%s\n", filename);
     if (file_pointer == NULL) {
-        puts("Error while opening the file");
+        printf("%s\n", strerror(errno));
         exit(2);
     }
 
@@ -18,9 +18,12 @@ void open_bmp_file(const char filename[]) {
 //    printf("size of bmp header %u\n", sizeof(bmp_header)); // checking the size of bmp header
 
     fread(&bmp_header, sizeof(bmp_header), 1, file_pointer);
+    /*
     printf("%c%c field\n", bmp_header.header_field[0], bmp_header.header_field[1]);
     printf("size in bytes %d\n", bmp_header.header_size);
     printf("Offset: %d\n", bmp_header.image_offset);
+*/
+
 
     fclose(file_pointer);
 }
--- bmp.h
+++ bmp.h
@@ -12,12 +12,27 @@
 #include "stdlib.h"
 #endif //DSA_PROJECT_BMP_H
 
-/*
+/* WARNING!!! Using C99 standard of C!
  * Using structure packing technique instead of
  * structure padding in order to eliminate wastage of
  * memory, although there will be wastage of CPU cycles
  * which is not primary concern in out cases.
+ *
+ * If the attribute((packed)) doesn't work,
+ * instead you can include #pragma pack(1) directive
+ * after #define DSA_PROJECT_BMP_H
+ *
+ * Concerning uint_fast32_t and other types:
+ *  Fastest minimum-width integer types
+ *  Not actually guaranteed to be fastest for all purposes
+ *  Here we use the exact-width types for 8 and 16-bit ints.
+ *
  * */
+
+
+/*
+ * This block of bytes is at the start of the file and is used to identify the file.
+*/
 struct BITMAP_HEADER {
     uint_fast8_t header_field[2]; // for 'B' and 'M'
     uint_fast32_t header_size; // size of the bmp header
@@ -26,5 +41,18 @@ struct BITMAP_HEADER {
 } __attribute__((packed));
 
 typedef struct BITMAP_HEADER sBITMAP_HEADER;
+
+struct DIB_HEADER
+{
+    uint_fast32_t header_size;
+    uint_fast32_t bmp_width;
+    uint_fast32_t bmp_height;
+    uint_fast16_t  color_planes; // note: 2 bytes
+    uint_fast16_t bits_per_pixel; // note: 2 bytes
+    uint_fast32_t compression_method;
+    uint_fast32_t image_size;
+
+    uint_fast32_t other[4]; // not used
+};
 
 void open_bmp_file(const char filename[]);
