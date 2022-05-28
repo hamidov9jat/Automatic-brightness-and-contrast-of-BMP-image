33c140b6bb6d4d936d74b983d06c40c16475c1d8
Nijat Hamidov <hamidov9jat@gmail.com>
2022-05-28 10:20:26 +0400

    "Some ide changes"

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/User/Documents/Other/dsa L2/dsa_project
+# Install script for directory: C:/Users/User/Documents/Other/dsa L2/Automatic-brightness-and-contrast-of-BMP-image
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/User/Documents/Other/dsa L2/dsa_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/User/Documents/Other/dsa L2/Automatic-brightness-and-contrast-of-BMP-image/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
