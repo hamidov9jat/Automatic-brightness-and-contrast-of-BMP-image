ec47e451a1b4a724c6d7e15fdcabf6a5ddb5ef33
Nijat Hamidov <hamidov9jat@gmail.com>
2022-05-28 10:20:49 +0400

    Unresolved files commit

---
+++ cmake-build-debug/CMakeFiles/dsa_project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsa_project.dir/bmp.c.obj"
+  "CMakeFiles/dsa_project.dir/bmp.c.obj.d"
+  "CMakeFiles/dsa_project.dir/main.c.obj"
+  "CMakeFiles/dsa_project.dir/main.c.obj.d"
+  "dsa_project.exe"
+  "dsa_project.exe.manifest"
+  "dsa_project.pdb"
+  "libdsa_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dsa_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
